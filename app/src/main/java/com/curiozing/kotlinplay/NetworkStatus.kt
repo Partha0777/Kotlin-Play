2270de23ab7e18dfb7e6717d6acf14d287bbd69a
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2024-04-18 20:29:39 +0530

    Just tried with interface classes like sealed class.

--- app/src/main/java/com/curiozing/kotlinplay/NetworkStatus.kt
+++ app/src/main/java/com/curiozing/kotlinplay/NetworkStatus.kt
@@ -3,58 +3,62 @@ package com.curiozing.kotlinplay
 import okhttp3.ResponseBody
 
 sealed class NetworkStatus(var data: String) {
-    class Success(var response:ResponseBody): NetworkStatus(""){
-        fun well(){
+    class Success(var response: ResponseBody) : NetworkStatus("") {
+        fun well() {
 
         }
     }
-    class Failure(var errorCode:Int, var errorMessage:String): NetworkStatus(""){
-        fun hello(){
+
+    class Failure(var errorCode: Int, var errorMessage: String) : NetworkStatus("") {
+        fun hello() {
 
         }
     }
-    data object Loading: NetworkStatus("")
+
+    data object Loading : NetworkStatus("")
 
 }
 
 
-interface DummyData{
+interface DummyData {
 
 }
 
-class  Imple1 (var data:String): DummyData {
+class Imple1(var data: String) : DummyData {
 
 }
 
-class  Imple2 : DummyData {
+class Imple2 : DummyData {
 
 }
 
-class Base{
+class Base {
 
-    fun getting(io:DummyData){
-      when(io){
-          is Imple1 -> {
-          }
-      }
-    }
-
-    fun getting(io:NetworkStatus){
-        (
-            when (io) {
-                is NetworkStatus.Failure ->{
-
-                    io.hello()
-                }
-                is NetworkStatus.Success ->{
-
-                    io.well()
-                }
-                is NetworkStatus.Loading ->{
-
-                }
+    fun getting(io: DummyData) {
+        when (io) {
+            is Imple1 -> {
             }
-        )
+        }
+    }
+
+    fun getting(io: NetworkStatus) {
+        (
+                when (io) {
+                    is NetworkStatus.Failure -> {
+
+                        io.hello()
+                    }
+
+                    is NetworkStatus.Success -> {
+
+                        io.well()
+                    }
+
+                    is NetworkStatus.Loading -> {
+
+                    }
+                }
+                )
     }
 
 }
