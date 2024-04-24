ceddeac157a93f0e74bd4ce0ca244ec2fc820952
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2024-04-24 23:49:25 +0530

    Worked out kotlin delegation with example.

---
+++ app/src/main/java/com/curiozing/kotlinplay/oops/DelegationExample.kt
@@ -0,0 +1,42 @@
+package com.curiozing.kotlinplay.oops
+
+fun main() {
+
+    val headPhone = HeadPhone()
+    val speaker = Speaker()
+    MusicPlayer(headPhone).play()
+}
+
+interface Music{
+    fun play()
+
+    fun pause()
+}
+
+class HeadPhone : Music{
+    override fun play() {
+        TODO("Not yet implemented")
+    }
+
+    override fun pause() {
+        TODO("Not yet implemented")
+    }
+
+    fun pauseByButton(){
+
+    }
+
+}
+
+class Speaker : Music{
+    override fun play() {
+        TODO("Not yet implemented")
+    }
+
+    override fun pause() {
+        TODO("Not yet implemented")
+    }
+
+}
+
+class MusicPlayer(music: Music): Music by music
