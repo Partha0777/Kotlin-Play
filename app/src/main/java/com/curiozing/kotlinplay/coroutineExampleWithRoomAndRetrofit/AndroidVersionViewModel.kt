68d3a9fdee5ba7ecda3eec9ecc5c03e6ce82537f
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2024-05-04 21:20:57 +0530

    Validated database before call the API for show data from in offline mode.

--- app/src/main/java/com/curiozing/kotlinplay/coroutineExampleWithRoomAndRetrofit/AndroidVersionViewModel.kt
+++ app/src/main/java/com/curiozing/kotlinplay/coroutineExampleWithRoomAndRetrofit/AndroidVersionViewModel.kt
@@ -1,7 +1,5 @@
 package com.curiozing.kotlinplay.coroutineExampleWithRoomAndRetrofit
 
-import android.util.Log
-import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
--- app/src/main/java/com/curiozing/kotlinplay/coroutineExampleWithRoomAndRetrofit/ApiHelper.kt
+++ app/src/main/java/com/curiozing/kotlinplay/coroutineExampleWithRoomAndRetrofit/ApiHelper.kt
@@ -1,15 +1,13 @@
 package com.curiozing.kotlinplay.coroutineExampleWithRoomAndRetrofit
 
-import com.google.gson.Gson
 import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.create
 
 object ApiHelper {
 
-    private fun client () : Retrofit = Retrofit.Builder().client(OkHttpClient())
+    private fun client(): Retrofit = Retrofit.Builder().client(OkHttpClient())
         .baseUrl("https://kotlin-play-default-rtdb.firebaseio.com")
         .addConverterFactory(GsonConverterFactory.create())
         .addCallAdapterFactory(CoroutineCallAdapterFactory()).build()
