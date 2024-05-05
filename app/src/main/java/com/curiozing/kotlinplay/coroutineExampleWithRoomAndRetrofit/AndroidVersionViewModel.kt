a426d69a330ee334e5ab3a01bc4571402e1d7a04
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2024-05-05 17:21:10 +0530

    Database sync with remote data completed.

--- app/src/main/java/com/curiozing/kotlinplay/coroutineExampleWithRoomAndRetrofit/AndroidVersionViewModel.kt
+++ app/src/main/java/com/curiozing/kotlinplay/coroutineExampleWithRoomAndRetrofit/AndroidVersionViewModel.kt
@@ -15,13 +15,35 @@ class AndroidVersionViewModel : ViewModel() {
     fun getAndroidVersions() {
         val db = (MyApplication.getContext() as MyApplication).getAppDatabase()
         viewModelScope.launch {
-           var data = db.versionDAO().getVersions()
-            if (data.isNotEmpty()){
-                uiState.value = UiState.Success(data)
+            val localData = db.versionDAO().getVersions().toMutableList()
+            when {
+                localData.isNotEmpty() -> {
+                    uiState.value = UiState.Success(localData)
+                }
             }
-            data = ApiHelper.retrofitClient.getAndroidVersion()
-            db.versionDAO().insertAllVersion(data)
-            uiState.value = UiState.Success(data)
+
+
+
+            val remoteData = ApiHelper.retrofitClient.getAndroidVersion()
+
+          remoteData.forEachIndexed{ index, version ->
+              if (localData.getOrNull(index) == null){
+                  db.versionDAO().insertVersion(version)
+              }else{
+                  remoteData[index].id = localData[index].id
+              }
+          }
+
+            when {
+                localData.isNotEmpty() -> {
+                    db.versionDAO().updateAllVersions(remoteData)
+                }
+                else -> {
+                    db.versionDAO().insertAllVersion(remoteData)
+                }
+            }
+
+            uiState.value = UiState.Success(remoteData)
         }
 
     }
--- app/src/main/java/com/curiozing/kotlinplay/coroutineExampleWithRoomAndRetrofit/VersionDAO.kt
+++ app/src/main/java/com/curiozing/kotlinplay/coroutineExampleWithRoomAndRetrofit/VersionDAO.kt
@@ -4,16 +4,20 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Update
 
 
 @Dao
 interface VersionDAO {
 
     @Insert
-    fun insertVersion(version: Version)
+    suspend fun insertVersion(version: Version)
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertAllVersion(versions: List<Version>)
+
+    @Update(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun updateAllVersions(versions: List<Version>)
 
     @Query("SELECT * FROM androidVersion")
     suspend fun getVersions(): List<Version>
