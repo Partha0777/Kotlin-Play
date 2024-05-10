1f1d6caa181e6a1976ea959265346a99bab30f75
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2024-05-10 23:17:48 +0530

    Added some example higher order functions using the generic types.

--- app/src/main/java/com/curiozing/kotlinplay/kotlinVsJava/conciseness/ConcisenessInKotlin.kt
+++ app/src/main/java/com/curiozing/kotlinplay/kotlinVsJava/conciseness/ConcisenessInKotlin.kt
@@ -1,15 +1,16 @@
 package com.curiozing.kotlinplay.kotlinVsJava.conciseness
 
 
-fun main(){
+fun main() {
     val conciseness = ConcisenessInKotlin()
     conciseness.loadList()
 }
+
 class ConcisenessInKotlin {
 
-    fun loadList(){
-        val data = listOf(1,2,3,4,5)
-        val  evenNumbers = data.filter { it % 2 == 0 }
+    fun loadList() {
+        val data = listOf(1, 2, 3, 4, 5)
+        val evenNumbers = data.filter { it % 2 == 0 }
         println(evenNumbers)
     }
 }
