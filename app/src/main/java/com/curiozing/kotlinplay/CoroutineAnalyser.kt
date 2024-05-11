0353f4fad469017a26bfc49bb4325bfff426b5e7
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2024-05-11 22:01:07 +0530

    Added new screen for coroutine analyser.

--- app/src/main/java/com/curiozing/kotlinplay/CoroutineAnalyser.kt
+++ app/src/main/java/com/curiozing/kotlinplay/CoroutineAnalyser.kt
@@ -1,16 +1,25 @@
 package com.curiozing.kotlinplay
 
+import android.widget.EditText
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 
 
 @Composable
 fun CoroutineAnalyser(){
     Surface {
-        Column {
-            Text(text = "Helloooooo")
+        Column(
+            modifier = Modifier.fillMaxWidth().fillMaxHeight(),
+            horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
+            Text(text = "Enter Number: ")
         }
     }
 }
