9f4d021bf8bf9230f99d9883cb4c58b2ff68ab74
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2024-05-17 22:43:13 +0530

    Added coroutine supervisor job example.

--- app/src/main/java/com/curiozing/kotlinplay/kotlinVsJava/coroutine/CoroutineExample.kt
+++ app/src/main/java/com/curiozing/kotlinplay/kotlinVsJava/coroutine/CoroutineExample.kt
@@ -2,10 +2,11 @@ package com.curiozing.kotlinplay.kotlinVsJava.coroutine
 
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class CoroutineExample {
     fun startCoroutine(){
-        CoroutineScope(Dispatchers.Main)
+        CoroutineScope(Dispatchers.Main).launch {  }
 
     }
 }
