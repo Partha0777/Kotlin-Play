47921fa32fd15f555661d04421d643cc31faf721
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2024-05-19 22:30:03 +0530

    Added example for how the coroutine jobs will run sync and async way.

--- app/src/main/java/com/curiozing/kotlinplay/kotlinVsJava/coroutine/CoroutineExample.kt
+++ app/src/main/java/com/curiozing/kotlinplay/kotlinVsJava/coroutine/CoroutineExample.kt
@@ -2,8 +2,11 @@ package com.curiozing.kotlinplay.kotlinVsJava.coroutine
 
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.cancel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlin.ClassCastException
 
 class CoroutineExample {
     fun startCoroutine() {
@@ -15,26 +18,56 @@ class CoroutineExample {
 }
 
 fun main() {
-    val job = CoroutineScope(Dispatchers.IO)
+    val job = CoroutineScope(Dispatchers.IO + SupervisorJob())
+    //val job = CoroutineScope(Dispatchers.IO)
 
 
     job.launch {
 
-        callAnotherFunction()
+
+        /*val job1 = CoroutineScope(Dispatchers.IO)
+
+            job1.launch {
+            delay(100)
+            println("Cancel Running job 1")
+        }
+
+        val job2 = CoroutineScope(Dispatchers.IO)
+
+            job2.launch {
+            delay(100)
+            println("Cancel Running job 2")
+        }*/
+
+
+
 
         launch {
             delay(200)
             println("Running job 1")
+            throw ClassCastException()
+
         }
+        callAnotherFunction()
 
         launch {
             delay(100)
             println("Running job 2")
+
         }
 
         launch {
             delay(400)
             println("Running job 3")
+        }
+
+    }
+
+    job.launch {
+        launch {
+            delay(500)
+            println("Another Running job 2")
+
         }
 
     }
