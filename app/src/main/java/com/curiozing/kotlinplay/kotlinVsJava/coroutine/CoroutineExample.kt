1396ef4c8d0429e2267351e3fe988bab07724809
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2024-05-19 20:53:59 +0530

    Added example for how the coroutine jobs will run sync and async way.

--- app/src/main/java/com/curiozing/kotlinplay/kotlinVsJava/coroutine/CoroutineExample.kt
+++ app/src/main/java/com/curiozing/kotlinplay/kotlinVsJava/coroutine/CoroutineExample.kt
@@ -6,7 +6,7 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class CoroutineExample {
-    fun startCoroutine(){
+    fun startCoroutine() {
         CoroutineScope(Dispatchers.Main).launch {
 
 
