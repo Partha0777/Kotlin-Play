8d25b625497c652ded667b2f44b60c43339237fd
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2024-05-24 22:32:42 +0530

    Added example for coroutine exception handler.

---
+++ app/src/main/java/com/curiozing/kotlinplay/coroutineExampleWithRoomAndRetrofit/CoroutineExceptionHandle.kt
@@ -0,0 +1,24 @@
+package com.curiozing.kotlinplay.coroutineExampleWithRoomAndRetrofit
+
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+
+fun main() {
+
+    val coroutineException = CoroutineExceptionHandler { coroutineContext, throwable ->
+        println("Coroutine Exception: $throwable")
+    }
+    val job = CoroutineScope(Job())
+
+    job.launch {
+
+        launch(coroutineException) {
+            throw RuntimeException()
+        }
+
+    }
+
+    Thread.sleep(500)
+}
