22c660555c77a742bd2d853f850c47195112ee9f
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2024-05-26 13:07:47 +0530

    Added example for supervisor scope.

--- app/src/main/java/com/curiozing/kotlinplay/kotlinVsJava/coroutine/CoroutineExceptionForSupervisorScope.kt
+++ app/src/main/java/com/curiozing/kotlinplay/kotlinVsJava/coroutine/CoroutineExceptionForSupervisorScope.kt
@@ -3,6 +3,7 @@ package com.curiozing.kotlinplay.kotlinVsJava.coroutine
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.supervisorScope
@@ -26,13 +27,22 @@ fun main() {
 
     job.launch{
 
+        supervisorScope {
             launch {
                 throw RuntimeException()
             }
             launch {
-                println("Helooo")
+                delay(200)
+                println("Helooo 1")
+
             }
 
+        }
+
+    }
+
+    job.launch {
+        println("Helooo 2")
     }
 
 
