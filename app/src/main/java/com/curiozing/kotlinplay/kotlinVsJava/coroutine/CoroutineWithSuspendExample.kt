7d97a7ac99708c1f15dc12fbb8a9cd5c13f3b232
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2024-05-26 11:27:33 +0530

    Added example for suspend function with launch in coroutine.

--- app/src/main/java/com/curiozing/kotlinplay/kotlinVsJava/coroutine/CoroutineWithSuspendExample.kt
+++ app/src/main/java/com/curiozing/kotlinplay/kotlinVsJava/coroutine/CoroutineWithSuspendExample.kt
@@ -11,18 +11,20 @@ fun main() {
 
     scope.launch {
         launch {
+            delay(1000)
             println("Line 1")
         }
 
         callOuterFunction()
 
         launch {
+            delay(200)
             println("Line 2")
         }
 
     }
 
-    Thread.sleep(2000)
+    Thread.sleep(3000)
 }
 
 suspend fun callOuterFunction(){
