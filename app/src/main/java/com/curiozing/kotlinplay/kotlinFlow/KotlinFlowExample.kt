2bea721230d1d551662ca850ecbbe3019d31c864
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2024-05-30 22:18:49 +0530

    Added calculate Factorial functionality.

--- app/src/main/java/com/curiozing/kotlinplay/kotlinFlow/KotlinFlowExample.kt
+++ app/src/main/java/com/curiozing/kotlinplay/kotlinFlow/KotlinFlowExample.kt
@@ -3,6 +3,7 @@ package com.curiozing.kotlinplay.kotlinFlow
 import java.math.BigInteger
 
 fun main(){
+    println("factorial... ${calculateFactorial(5)}")
 }
 
 fun calculateFactorial(number:Int):BigInteger{
