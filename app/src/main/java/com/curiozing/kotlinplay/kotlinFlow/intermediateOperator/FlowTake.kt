6c2e4f0e33128ccdfbb6ea7709f2377bf38d9b58
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2024-06-11 11:07:20 +0530

    Added example for flow take intermediate operator.

---
+++ app/src/main/java/com/curiozing/kotlinplay/kotlinFlow/intermediateOperator/FlowTake.kt
@@ -0,0 +1,13 @@
+package com.curiozing.kotlinplay.kotlinFlow.intermediateOperator
+
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.take
+
+suspend fun main() {
+    flowOf(1,2,3,4,5)
+        .take(3)
+        .collect{
+        println("Take $it")
+    }
+}
