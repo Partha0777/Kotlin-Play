00ec77d2470e0b22c94af2f269c2de084cdf4d47
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2024-06-12 22:44:31 +0530

    Added example for flow drop intermediate operator.

---
+++ app/src/main/java/com/curiozing/kotlinplay/kotlinFlow/intermediateOperator/FlowDrop.kt
@@ -0,0 +1,13 @@
+package com.curiozing.kotlinplay.kotlinFlow.intermediateOperator
+
+import kotlinx.coroutines.flow.drop
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.take
+
+suspend fun main() {
+    flowOf(1,2,3,4,5)
+        .drop(3)
+        .collect{
+            println("Take $it")
+        }
+}
