fe99ad310ff5b42cb0c215861d6aed89d8b51907
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2024-06-13 23:07:29 +0530

    Added example for flow transform intermediate operator.

---
+++ app/src/main/java/com/curiozing/kotlinplay/kotlinFlow/intermediateOperator/FlowTransform.kt
@@ -0,0 +1,19 @@
+package com.curiozing.kotlinplay.kotlinFlow.intermediateOperator
+
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.transform
+
+
+suspend fun main(){
+
+    flowOf(1,2,3,4,5,6,7,8)
+        .transform{
+           if(it % 2 == 0){
+               emit(it)
+           }
+        }
+        .collect{
+            println("Data $it")
+        }
+}
