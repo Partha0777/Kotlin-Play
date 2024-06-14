c8a3be4599e4b84017352303422413a57d4d982f
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2024-06-14 18:37:25 +0530

    Added example for flow fliter intermediate operator.

---
+++ app/src/main/java/com/curiozing/kotlinplay/kotlinFlow/intermediateOperator/FlowFilter.kt
@@ -0,0 +1,16 @@
+package com.curiozing.kotlinplay.kotlinFlow.intermediateOperator
+
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.flowOf
+
+suspend fun main(){
+
+    flowOf(1,2,3,4,5,6,7)
+        .filter {
+            it % 2 == 0
+        }
+        .collect{
+            println("Data $it")
+        }
+}
