e7756748117a08d34881da8a0e9c3506fea326c4
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2024-06-16 15:55:03 +0530

    Added example for cricket player batting and bowling calculations.

---
+++ app/src/main/java/com/curiozing/kotlinplay/cricketPoints/CricketPointsCalculations.kt
@@ -0,0 +1,127 @@
+package com.curiozing.kotlinplay.cricketPoints
+
+import kotlin.math.roundToInt
+
+fun normalize(value: Double, maxValue: Double): Double {
+    return (value / maxValue) * 100
+}
+
+fun calculatePoints(criteria: List<Double>, weightage: List<Double>): Double {
+    return criteria.zip(weightage).sumByDouble { (c, w) -> c * w }
+}
+enum class Role {
+    Batsman,
+    Bowler,
+    AllRounder
+}
+data class Player(
+    var name:String,
+    var battingAvg: Double,
+    var totalRuns: Double,
+    var strikeRate: Double,
+    var centuriesAndFifties: Double,
+    var bowlingAvg: Double,
+    var totalWickets: Double,
+    var economyRate: Double,
+    var fiveWicketsHauls: Double,
+    var role: Role,
+    )
+
+fun main() {
+
+    var virat = Player(name = "Virat Kohli",
+        49.9,
+        4042.0,
+        137.91,
+        38.0,
+        51.0,
+        4.0,
+        8.05,
+        0.0,
+        Role.Batsman
+    )
+
+    var rohit = Player(name = "Rohit Sharma",
+        31.09,
+        4042.0,
+        139.67,
+        35.0,
+        113.0,
+        1.0,
+        9.97,
+        0.0,
+        Role.Batsman
+    )
+
+    var bumrah = Player(name = "Jasprit Bumrah",
+        2.67,
+        8.0,
+        57.14,
+        0.0,
+        18.99,
+        79.0,
+        6.44,
+        0.0,
+        Role.Bowler
+    )
+    var bumrahIpl = Player(name = "Jasprit Bumrah",
+        2.67,
+        8.0,
+        57.14,
+        0.0,
+        22.52,
+        165.0,
+        7.3,
+        2.0,
+        Role.Bowler
+    )
+
+    var ashwin = Player(name = "Ravichandran Ashwin",
+        26.29,
+        184.0,
+        115.0,
+        0.0,
+        23.22,
+        72.0,
+        6.91,
+        0.0,
+        Role.AllRounder
+    )
+
+
+    val player:Player = bumrahIpl
+
+    val battingCriteria = listOf(
+        normalize(player.battingAvg, 60.0),  // Bowling Average
+        normalize(player.totalRuns, 10000.0),  // Total Runs
+        normalize(135.0.coerceAtMost(100.0), 100.0),  // Strike Rate (Capped at 100)
+        normalize(player.centuriesAndFifties, 30.0)  // Centuries and Fifties
+    )
+
+    val battingWeightage = listOf(0.3, 0.4, 0.3, 0.1)
+
+    var battingPoints = calculatePoints(battingCriteria, battingWeightage)
+    if(player.role == Role.Bowler){
+        battingPoints /= 2
+    }
+    println("${player.name} T20 Batting Points: ${battingPoints.roundToInt()}")
+
+
+
+    val bowlingCriteria = listOf(
+        normalize(100 - player.bowlingAvg, 100.0),  // Bowling Average
+        normalize(player.totalWickets, 200.0),  // Total Wickets
+        normalize(36 - player.economyRate, 36.0),  // Economy Rate
+        normalize(player.fiveWicketsHauls, 5.0)   // Five-Wicket Hauls
+    )
+
+    val bowlingWeightage = listOf(0.4, 0.3, 0.3, 0.1)
+
+    var bowlingPoints = calculatePoints(bowlingCriteria, bowlingWeightage)
+    if (player.role == Role.Batsman){
+        bowlingPoints /= 2
+    }
+    println("${player.name} T20 Bowling Points: ${bowlingPoints.roundToInt()}")
+
+}
+
--- app/src/main/java/com/curiozing/kotlinplay/kotlinFlow/FlowViewModelExample.kt
+++ app/src/main/java/com/curiozing/kotlinplay/kotlinFlow/FlowViewModelExample.kt
@@ -3,13 +3,16 @@ package com.curiozing.kotlinplay.kotlinFlow
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
 import com.curiozing.kotlinplay.coroutineExampleWithRoomAndRetrofit.ApiHelper
 import com.curiozing.kotlinplay.coroutineExampleWithRoomAndRetrofit.Time
 import com.curiozing.kotlinplay.coroutineExampleWithRoomAndRetrofit.UiState
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onCompletion
 import kotlinx.coroutines.flow.onStart
@@ -24,6 +27,8 @@ class FlowViewModelExample : ViewModel() {
         }
     }
 
+
+
     //Map is the intermediate operator and asLiveData is terminal Operator
     var uiState: LiveData<UiState> = currentTime.map {
         UiState.LoadTime(it) as UiState
@@ -32,4 +37,19 @@ class FlowViewModelExample : ViewModel() {
     }.onCompletion {
 
     }.asLiveData(Dispatchers.Default)
+
+    fun launchIn(){
+        currentTime.map {
+            UiState.LoadTime(it) as UiState
+        }.onStart {
+            emit(UiState.Loading)
+        }.onCompletion {
+
+        }.launchIn(viewModelScope)
+    }
+
+
+
+
+
 }
