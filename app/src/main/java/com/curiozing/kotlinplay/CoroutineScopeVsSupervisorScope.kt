78920375f734531c9f709bb353c8334980405af7
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2024-06-19 21:15:32 +0530

    Added simple example for differentiate the coroutine scope and supervisor scope.

--- app/src/main/java/com/curiozing/kotlinplay/CoroutineScopeVsSupervisorScope.kt
+++ app/src/main/java/com/curiozing/kotlinplay/CoroutineScopeVsSupervisorScope.kt
@@ -2,6 +2,7 @@ package com.curiozing.kotlinplay
 
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
@@ -23,5 +24,23 @@ suspend fun main() {
         println("Task 3")
     }
 
-    delay(1000)
+
+    var supervisorJob = CoroutineScope(SupervisorJob() +Dispatchers.Default)
+
+    supervisorJob.launch {
+        delay(100)
+        println("Super Task 1")
+    }
+
+    supervisorJob.launch {
+        delay(200)
+        println("Super Task 2")
+        throw RuntimeException()
+    }
+
+    supervisorJob.launch {
+        delay(300)
+        println("Super Task 3")
+    }
+    delay(2000)
 }
