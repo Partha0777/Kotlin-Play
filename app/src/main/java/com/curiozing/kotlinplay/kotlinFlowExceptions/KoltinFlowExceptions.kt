947bd6637d7698e89852667c7a1c359e9fc479e2
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2024-06-21 22:45:12 +0530

    Flow with normal try catch exceptions.

--- app/src/main/java/com/curiozing/kotlinplay/kotlinFlowExceptions/KoltinFlowExceptions.kt
+++ app/src/main/java/com/curiozing/kotlinplay/kotlinFlowExceptions/KoltinFlowExceptions.kt
@@ -1,14 +1,32 @@
 package com.curiozing.kotlinplay.kotlinFlowExceptions
 
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.onCompletion
+import kotlinx.coroutines.launch
 
-fun main() {
-
-
+suspend fun main(){
+    try {
+        data().onCompletion { cause ->
+            if (cause == null){
+                println("Completed Normally")
+            }
+        }.collect{
+            println(it)
+        }
+    }catch (e:Exception){
+        print("Error is $e")
+    }
 }
 
 fun data(): Flow<String> = flow {
     emit("Hello")
+    delay(500)
+    emit("Hello1")
+    delay(500)
+    emit("Hello2")
+    throw Exception("Error")
 }
