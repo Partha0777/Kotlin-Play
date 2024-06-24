080f6c13682ea20792eddc05f7a81af8afc2c747
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2024-06-24 18:38:25 +0530

    Added example for coroutine cooperative regarding exception with isActive method

--- app/src/main/java/com/curiozing/kotlinplay/MainViewModel.kt
+++ app/src/main/java/com/curiozing/kotlinplay/MainViewModel.kt
@@ -3,6 +3,8 @@ package com.curiozing.kotlinplay
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.delay
@@ -16,12 +18,19 @@ import java.time.Duration
 class MainViewModel : ViewModel() {
 
     fun getDrinks() {
-        viewModelScope.launch {
+        CoroutineScope(Dispatchers.Main.immediate).launch {
             println("Viewmodel called first")
             launch {
+                Log.d("Response ", "Launch 1")
                 val response1 = NetworkHelper.drinksService.getHotCoffee()
                 Log.d("Response1 ", response1.string())
             }
+            launch {
+                Log.d("Response ", "Launch 2")
+                val response1 = NetworkHelper.drinksService.getHotCoffee()
+                Log.d("Response2 ", response1.string())
+            }
+
 
             Log.d("Response ", "Called 1")
             val response = NetworkHelper.drinksService.getHotCoffee()
---
+++ app/src/main/java/com/curiozing/kotlinplay/coroutine/CoOperativeRegardingCancellations.kt
@@ -0,0 +1,29 @@
+package com.curiozing.kotlinplay.coroutine
+
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.ensureActive
+import kotlinx.coroutines.isActive
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.yield
+
+fun main() = runBlocking {
+
+    val job = launch(Dispatchers.IO) {
+        repeat(5){
+           if (isActive){
+               println("Its $it time")
+               Thread.sleep(300)
+           }else{
+               println("Coroutine is Cancelled")
+               throw  CancellationException()
+           }
+
+        }
+    }
+    delay(200)
+    job.cancel()
+
+}
