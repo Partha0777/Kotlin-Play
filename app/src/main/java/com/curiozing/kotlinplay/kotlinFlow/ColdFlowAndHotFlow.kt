abf8c05a51478bfe9110220d9b41bea897ecd6c8
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2024-06-25 13:23:23 +0530

    Added example for Cold flow and Hot flow.

--- app/src/main/java/com/curiozing/kotlinplay/kotlinFlow/ColdFlowAndHotFlow.kt
+++ app/src/main/java/com/curiozing/kotlinplay/kotlinFlow/ColdFlowAndHotFlow.kt
@@ -1,27 +1,51 @@
 package com.curiozing.kotlinplay.kotlinFlow
 
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import java.util.concurrent.Flow
-fun main() = runBlocking{
+fun main() = runBlocking(Dispatchers.Default){
     launch {
         coldFlow().collect{
-            println("Collector 1 - $it")
+            println("Cold Collector 1 - $it")
         }
 
     }
 
     launch {
         coldFlow().collect{
-            println("Collector 2 - $it")
+            println("Cold Collector 2 - $it")
         }
     }
 
-    delay(300)
+    val job = CoroutineScope(Dispatchers.Default)
+    val hotFlow = MutableSharedFlow<String>()
 
+    job.launch {
+        repeat(5){
+            println("Hot FLow $it")
+            delay(200)
+            hotFlow.emit("Hot $it")
+        }
+    }
 
+    job.launch {
+        hotFlow.collect{
+            println("Hot Collector 1 - $it")
+        }
+    }
+
+    job.launch {
+        hotFlow.collect{
+            println("Hot Collector 2 - $it")
+        }
+    }
+
+    delay(6000)
 
 }
 
