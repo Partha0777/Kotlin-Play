80d3dceee51358c39c56db429bfede62745fb2e8
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2024-06-26 21:51:53 +0530

    Added one more delegation example in kotlin.

---
+++ app/src/main/java/com/curiozing/kotlinplay/delegation/DelegationExample2.kt
@@ -0,0 +1,33 @@
+package com.curiozing.kotlinplay.delegation
+
+class DelegationExample2: Analytics by ImplAnalytics(), Logs by ImplLogs() {
+
+    fun onCreate(){
+        recordAnalytics("OnStart")
+
+        logData("Error occurred!")
+    }
+}
+
+interface Analytics{
+
+    fun recordAnalytics(data: String)
+}
+
+
+interface Logs{
+
+    fun logData(data: String)
+}
+
+
+open class ImplAnalytics: Analytics{
+    override fun recordAnalytics(data: String) {
+    }
+
+}
+
+class ImplLogs: Logs{
+    override fun logData(data: String) {
+    }
+}
---
+++ app/src/main/java/com/curiozing/kotlinplay/delegation/NewDelegationExample.kt
@@ -0,0 +1,42 @@
+package com.curiozing.kotlinplay.delegation
+
+fun main() {
+
+    val file = "demo.mp4"
+
+    val downloader = MediaDownloader(file)
+    val player = MediaPlayer(file)
+
+    val mediaFile = MediaFile(downloader = downloader, player = player)
+
+    mediaFile.download()
+    mediaFile.play()
+
+}
+
+
+
+class MediaFile(private val downloader: Downloader,private val player: Player):Downloader by downloader, Player by player
+
+interface Downloader{
+    fun download()
+}
+
+interface Player{
+    fun play()
+}
+
+
+class MediaDownloader(var file:String): Downloader{
+    override fun download() {
+        println("$file Downloading....")
+    }
+
+}
+
+class MediaPlayer(var file:String) : Player{
+    override fun play() {
+        println("$file Playing....")
+    }
+
+}
