3d404e13829aeea89f46e60dc5a54c8eea1dfb9e
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2024-06-29 21:42:12 +0530

    Added example for MutableStateflow

--- app/src/main/java/com/curiozing/kotlinplay/delegation/DelegationExample2.kt
+++ app/src/main/java/com/curiozing/kotlinplay/delegation/DelegationExample2.kt
@@ -1,33 +1,33 @@
 package com.curiozing.kotlinplay.delegation
 
-class DelegationExample2: Analytics by ImplAnalytics(), Logs by ImplLogs() {
+class DelegationExample2 : Analytics by ImplAnalytics(), Logs by ImplLogs() {
 
-    fun onCreate(){
+    fun onCreate() {
         recordAnalytics("OnStart")
 
         logData("Error occurred!")
     }
 }
 
-interface Analytics{
+interface Analytics {
 
     fun recordAnalytics(data: String)
 }
 
 
-interface Logs{
+interface Logs {
 
     fun logData(data: String)
 }
 
 
-open class ImplAnalytics: Analytics{
+open class ImplAnalytics : Analytics {
     override fun recordAnalytics(data: String) {
     }
 
 }
 
-class ImplLogs: Logs{
+class ImplLogs : Logs {
     override fun logData(data: String) {
     }
 }
