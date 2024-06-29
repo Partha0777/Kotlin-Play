bab5a35e6db8665e45236bdf7ba29be82f5ac779
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2024-06-29 21:41:17 +0530

    Added example for MutableStateflow

--- app/src/main/java/com/curiozing/kotlinplay/coroutine/CoOperativeRegardingCancellations.kt
+++ app/src/main/java/com/curiozing/kotlinplay/coroutine/CoOperativeRegardingCancellations.kt
@@ -12,26 +12,26 @@ import kotlin.math.min
 
 fun main() = runBlocking {
 
-    val list = listOf(1,2,3,4,5,6,7,8)
-    val splitList:MutableList<List<Int>> = mutableListOf()
+    val list = listOf(1, 2, 3, 4, 5, 6, 7, 8)
+    val splitList: MutableList<List<Int>> = mutableListOf()
     val splitCount = 3
     var i = 0
-    while (i < list.size){
-        val split:List<Int> = list.subList(i, min(i+splitCount,list.size))
+    while (i < list.size) {
+        val split: List<Int> = list.subList(i, min(i + splitCount, list.size))
         i += splitCount
         splitList.add(split)
     }
     println("Splited ---> $splitList")
 
     val job = launch {
-        repeat(5){
-         /*  if (isActive){
-               println("Its $it time")
-               Thread.sleep(300)
-           }else{
-               println("Coroutine is Cancelled")
-               throw  CancellationException()
-           }*/
+        repeat(5) {
+            /*  if (isActive){
+                  println("Its $it time")
+                  Thread.sleep(300)
+              }else{
+                  println("Coroutine is Cancelled")
+                  throw  CancellationException()
+              }*/
 
             yield()
             //ensureActive()
