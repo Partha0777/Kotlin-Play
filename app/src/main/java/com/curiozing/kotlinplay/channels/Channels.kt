a80b1c84b52637799a8eb1da8a8587bdbb6a8ce3
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2024-07-01 20:03:53 +0530

    Added example for multiple consumer for channel.

--- app/src/main/java/com/curiozing/kotlinplay/channels/Channels.kt
+++ app/src/main/java/com/curiozing/kotlinplay/channels/Channels.kt
@@ -1,15 +1,20 @@
 package com.curiozing.kotlinplay.channels
 
+import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.channels.consumeEach
 import kotlinx.coroutines.channels.produce
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
-suspend fun main() = coroutineScope {
+@OptIn(ExperimentalCoroutinesApi::class)
+suspend fun main(): Unit = coroutineScope {
 
-    val channel = produce<Int> {
+    val channel = produce {
 
         send(10)
         delay(1000)
@@ -25,7 +30,39 @@ suspend fun main() = coroutineScope {
     }
 
 
-    channel.consumeEach {
-        println("Received $it")
+    launch {
+        channel.consumeEach {
+            println("Consumer 1 $it")
+        }
     }
+
+    launch {
+        channel.consumeEach {
+            println("Consumer 2 $it")
+        }
+    }
+
+/*
+    val  coroutineExceptionHandler = CoroutineExceptionHandler { coroutineContext, throwable ->
+        println("Exception is $throwable")
+    }
+    val coroutineScopeJob = CoroutineScope(Dispatchers.Default+coroutineExceptionHandler)
+
+    coroutineScopeJob.launch {
+        delay(500)
+        println("Job 1")
+    }
+
+    coroutineScopeJob.launch {
+        delay(700)
+        throw RuntimeException()
+        println("Job 2")
+    }
+
+    coroutineScopeJob.launch {
+        delay(1000)
+        println("Job 3")
+    }*/
+
+
 }
