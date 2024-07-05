a8d04d0eec3ece44b21c6ed2bb392822ccbdce40
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2024-07-05 22:11:13 +0530

    Added Example for serializable.

--- app/src/main/java/com/curiozing/kotlinplay/parcelableAndSerializable/DataSerializable.kt
+++ app/src/main/java/com/curiozing/kotlinplay/parcelableAndSerializable/DataSerializable.kt
@@ -2,4 +2,4 @@ package com.curiozing.kotlinplay.parcelableAndSerializable
 
 import java.io.Serializable
 
-data class DataSerializable(var name:String, var age:Int): Serializable
+data class DataSerializable(var name: String, var age: Int) : Serializable
