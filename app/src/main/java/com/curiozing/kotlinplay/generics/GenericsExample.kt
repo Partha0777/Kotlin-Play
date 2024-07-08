35ab4f307bba4ec29f19704ce8b45fa4369266c2
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2024-07-08 22:53:59 +0530

    Added Generic methid example.

--- app/src/main/java/com/curiozing/kotlinplay/generics/GenericsExample.kt
+++ app/src/main/java/com/curiozing/kotlinplay/generics/GenericsExample.kt
@@ -3,8 +3,16 @@ package com.curiozing.kotlinplay.generics
 
 fun main() {
 
+
+
 }
 class GenericsExample<T>(var list:List<T>) {
 
+    fun find(value: T, found : (T) -> Unit ){
+        val data = list.filter {
+            it == value
+        }
+        found(data.first())
+    }
 
 }
