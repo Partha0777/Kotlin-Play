f6f3664e3e9dd8112d27f3660d1cad34e559c014
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2024-07-09 22:41:02 +0530

    Added Generic method example.

--- app/src/main/java/com/curiozing/kotlinplay/generics/GenericsExample.kt
+++ app/src/main/java/com/curiozing/kotlinplay/generics/GenericsExample.kt
@@ -3,31 +3,31 @@ package com.curiozing.kotlinplay.generics
 
 fun main() {
 
-    val intData = listOf(1,2,3,4,5,6,7)
+    val intData = listOf(1, 2, 3, 4, 5, 6, 7)
     val stringData = listOf("Kan", "Partha", "Sarathy", "Curio")
 
-    GenericsExample(stringData).find("Sarathy"){
+    GenericsExample(stringData).find("Sarathy") {
         println("Found $it")
     }
 
-    val data = genericFunc(intData){
+    val data = genericFunc(intData) {
         it * 2
     }
 
     println("Data is $data")
 }
-fun <T,R> genericFunc(list: List<T>, func: (T) -> R) : List<R>{
+
+fun <T, R> genericFunc(list: List<T>, func: (T) -> R): List<R> {
     return list.map { e -> func(e) }
 }
-class GenericsExample<T>(var list:List<T>) {
 
-    fun find(value: T, found : (T) -> Unit ){
+class GenericsExample<T>(var list: List<T>) {
+
+    fun find(value: T, found: (T) -> Unit) {
         val data = list.filter {
             it == value
         }
         found(data.first())
     }
-
-
 
 }
