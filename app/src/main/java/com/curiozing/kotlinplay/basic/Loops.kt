01389d2a4036c57690d4e7624a2dca386fb5c726
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2024-07-11 22:51:10 +0530

    Added basic for loop step and downto example

--- app/src/main/java/com/curiozing/kotlinplay/basic/Loops.kt
+++ app/src/main/java/com/curiozing/kotlinplay/basic/Loops.kt
@@ -2,13 +2,21 @@ package com.curiozing.kotlinplay.basic
 
 fun main() {
 
-    var list = listOf(1,2,3,4,5)
-    var list1 = listOf("c","d","e","f","g","a","b")
+    val list = listOf(1,2,3,4,5)
+    val list1 = listOf("c","d","e","f","g","a","b")
 
     for (i in list){
         println(i)
     }
 
+    for (i in 3 downTo 1){
+        println("downTo $i")
+    }
+
+
+    for(i in 0..11 step 2){
+        println("step $i")
+    }
 
     list1.forEach {
         println(it)
