b7f531f7aee369f5773d8d2990c583b76e8f2c47
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2024-07-11 22:46:23 +0530

    Added basic for loop example

---
+++ app/src/main/java/com/curiozing/kotlinplay/basic/Loops.kt
@@ -0,0 +1,13 @@
+package com.curiozing.kotlinplay.basic
+
+fun main() {
+
+    var list = listOf(1,2,3,4,5)
+    var list1 = listOf("c","d","e","f","g","a","b")
+
+    for (i in list){
+        println(i)
+    }
+
+
+}
