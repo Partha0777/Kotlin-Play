73ead2896a9bedabe56aa811414575103bb8cb91
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2024-07-13 20:25:41 +0530

    Added key and value for loop with map example

--- app/src/main/java/com/curiozing/kotlinplay/basic/Loops.kt
+++ app/src/main/java/com/curiozing/kotlinplay/basic/Loops.kt
@@ -2,20 +2,22 @@ package com.curiozing.kotlinplay.basic
 
 fun main() {
 
-    val list = listOf(1,2,3,4,5)
-    val list1 = listOf("c","d","e","f","g","a","b")
+    val list = listOf(1, 2, 3, 4, 5)
+    val list1 = listOf("c", "d", "e", "f", "g", "a", "b")
     val map = mapOf(1 to "Partha", 2 to "Sarathy", 3 to "Kan")
 
-    for (i in list){
+
+    for (i in list) {
+
         println(i)
     }
 
-    for (i in 3 downTo 1){
+    for (i in 3 downTo 1) {
         println("downTo $i")
     }
 
 
-    for(i in 0..11 step 2){
+    for (i in 0..11 step 2) {
         println("step $i")
     }
 
@@ -23,12 +25,12 @@ fun main() {
         println(it)
     }
 
-    for ((index,value) in list.withIndex()){
+    for ((index, value) in list.withIndex()) {
         println("index is $index - Value $value")
 
     }
 
-    for ((key,value) in map){
+    for ((key, value) in map) {
         println("key is $key - Value is $value")
     }
 
