22ac74d83b0dcb6c278fd8fb79b48877f2233467
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2024-08-07 23:32:54 +0530

    Added some palindrome example.

--- app/src/main/java/com/curiozing/kotlinplay/kotlinFlow/SequenceKotlinFlowExample1.kt
+++ app/src/main/java/com/curiozing/kotlinplay/kotlinFlow/SequenceKotlinFlowExample1.kt
@@ -3,11 +3,24 @@ package com.curiozing.kotlinplay.kotlinFlow
 import java.math.BigInteger
 
 fun main() {
-     var startTime = System.currentTimeMillis()
+     val startTime = System.currentTimeMillis()
     calculateFactorialList(5).forEach {
         println("factorial...${System.currentTimeMillis() - startTime} - $it")
     }
-    println("Sync Only")
+    palindrome("Nitin")
+}
+
+fun palindrome(string: String){
+
+    var data =  string.lowercase().reversed()
+
+    if (data == string.lowercase()){
+        println("It's Palindrome")
+    }else{
+        println("It's not Palindrome")
+    }
+
+
 }
 
 private fun calculateFactorialList(number: Int): Sequence<BigInteger> = sequence{
