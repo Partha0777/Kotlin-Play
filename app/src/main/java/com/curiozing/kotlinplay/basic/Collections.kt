6e6e8e03e03f73df34fefb0bba6539fae4a5c44c
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2024-09-18 21:33:32 +0530

    Added collection mapIndexedNotNull example.

--- app/src/main/java/com/curiozing/kotlinplay/basic/Collections.kt
+++ app/src/main/java/com/curiozing/kotlinplay/basic/Collections.kt
@@ -9,13 +9,23 @@ fun main() {
     println(squared)
 
     //mapIndexed
-    val words = listOf("a", "bb", "ccc")
+    val words = listOf("a", "b", "c")
     val indexedWords = words.mapIndexed { index, value -> "$index: $value" }
-    println(indexedWords) // Output: [0: a, 1: bb, 2: ccc]
+    println(indexedWords)
+    // Output: [0: a, 1: b, 2: c]
 
-   //mapNotNull
+
+    //mapNotNull
     val numbers1 = listOf(1, 2, 3)
     val evenOrNull = numbers1.mapNotNull { if (it % 2 == 0) it else null }
     println(evenOrNull) // Output: [2]
+
+
+    val indexedEvenLength = words.mapIndexedNotNull { index, value ->
+        if (value.length % 2 == 0) "$index: $value" else null
+    }
+    println(indexedEvenLength) // Output: [1: bb]
+
+
 
 }
