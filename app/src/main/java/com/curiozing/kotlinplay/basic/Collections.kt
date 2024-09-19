40909785d671f0e0d52968cea27144bddff2ead7
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2024-09-19 22:46:21 +0530

    Added collection flatten example.

--- app/src/main/java/com/curiozing/kotlinplay/basic/Collections.kt
+++ app/src/main/java/com/curiozing/kotlinplay/basic/Collections.kt
@@ -1,12 +1,15 @@
 package com.curiozing.kotlinplay.basic
 
 fun main() {
+
     //* Mapping *//
 
     //map
     val numbers = listOf(1, 2, 3)
     val squared = numbers.map { it * it }
     println(squared)
+    // Output: [1, 4, 9]
+
 
     //mapIndexed
     val words = listOf("a", "b", "c")
@@ -18,22 +21,26 @@ fun main() {
     //mapNotNull
     val numbers1 = listOf(1, 2, 3)
     val evenOrNull = numbers1.mapNotNull { if (it % 2 == 0) it else null }
-    println(evenOrNull) // Output: [2]
+    println(evenOrNull)
+    // Output: [2]
 
 
     val indexedEvenLength = words.mapIndexedNotNull { index, value ->
         if (value.length % 2 == 0) "$index: $value" else null
     }
-    println(indexedEvenLength) // Output: [1: bb]
+    println(indexedEvenLength)
+    // Output: [1: bb]
 
 
     val nestedList = listOf(1..2, 3..4)
     val flatList = nestedList.flatMap { it -> it.map { it * 2 } }
-    println(flatList) // Output: [1, 2, 3, 4]
+    println(flatList)
+    // Output: [1, 2, 3, 4]
 
 
     val flattenList = nestedList.flatten()
-    println(flattenList) // Output: [1, 2, 3, 4]
+    println(flattenList)
+    // Output: [1, 2, 3, 4]
 
 }
 
