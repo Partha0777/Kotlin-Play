5996721fc9032224e0b0477288c521ae0ca906f9
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2024-09-28 20:26:06 +0530

    Added some generic function example.

--- app/src/main/java/com/curiozing/kotlinplay/basic/test.kt
+++ app/src/main/java/com/curiozing/kotlinplay/basic/test.kt
@@ -1,18 +1,18 @@
 package com.curiozing.kotlinplay.basic
 
 fun main() {
-   var data = listOf(listOf("Hello Ko", "Hey Ja"), listOf("Kotlin Wow", "Java Oh")).flatMap {
-       it.flatMap{
-           it.split(" ")
-       }
-   }
+    var data = listOf(listOf("Hello Ko", "Hey Ja"), listOf("Kotlin Wow", "Java Oh")).flatMap {
+        it.flatMap {
+            it.split(" ")
+        }
+    }
 
-    var data2 = calculate(listOf("1","2","3")){
+    var data2 = calculate(listOf("1", "2", "3")) {
         "$it ooo"
     }
-        print("data --> $data2")
+    print("data --> $data2")
 }
 
-fun <T,R> calculate(data:List<T>, func:(T) -> R):List<R>{
+fun <T, R> calculate(data: List<T>, func: (T) -> R): List<R> {
     return data.map { func(it) }.toList()
 }
