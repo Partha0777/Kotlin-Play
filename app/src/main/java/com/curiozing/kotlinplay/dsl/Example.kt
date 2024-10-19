a49d3652f00e3280bd511a4a06452be1550dea16
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2024-10-19 20:11:46 +0530

    Added some example for lambda with receiver.

---
+++ app/src/main/java/com/curiozing/kotlinplay/dsl/Example.kt
@@ -0,0 +1,20 @@
+package com.curiozing.kotlinplay.dsl
+
+
+class Home  {
+    fun p(name:String) {
+        println("Hello $name")
+    }
+}
+
+
+fun main() {
+    dodo {
+        p("Partha")
+    }
+}
+
+fun dodo(init: Home.() -> Unit){
+    val home = Home()
+    home.init()
+}
