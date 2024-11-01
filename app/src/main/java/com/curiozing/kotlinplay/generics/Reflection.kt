22930abd0c3bb3fbf2850e433961066ecd6a7d0f
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2024-11-01 20:01:57 +0530

    Added reflection for the functions.

--- app/src/main/java/com/curiozing/kotlinplay/generics/Reflection.kt
+++ app/src/main/java/com/curiozing/kotlinplay/generics/Reflection.kt
@@ -1,15 +1,23 @@
 package com.curiozing.kotlinplay.generics
 
+import kotlin.reflect.full.declaredFunctions
 import kotlin.reflect.full.declaredMemberProperties
-import kotlin.reflect.full.memberProperties
+import kotlin.reflect.full.functions
 import kotlin.reflect.jvm.isAccessible
 
 fun main() {
     val userData = User::class
-
+    val userInstance = User()
     userData.declaredMemberProperties.forEach {
         it.isAccessible = true
         println(it.get(User()))
+    }
+
+    userData.declaredFunctions.forEach {
+        if (it.name == "getUserDetails"){
+            it.isAccessible = true
+            println(it.call(userInstance,"Hello, I'm"))
+        }
     }
 }
 
@@ -17,8 +25,8 @@ class User{
     private val name = "Partha"
     private val city = "Chennai"
 
-    fun getUserDetails():String{
-        return "$name - $city"
+    private fun getUserDetails(greet:String):String{
+        return "$greet $name - $city"
     }
 
 }
