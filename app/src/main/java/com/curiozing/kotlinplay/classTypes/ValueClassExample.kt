b4a38c3b3d2631c2b788ea064455ea668ae433b4
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2024-11-29 20:43:51 +0530

    Added some example for Value class.

---
+++ app/src/main/java/com/curiozing/kotlinplay/classTypes/ValueClassExample.kt
@@ -0,0 +1,21 @@
+package com.curiozing.kotlinplay.classTypes
+
+
+fun main() {
+    sendEmail(EmailId("rps@gmail.com"))
+}
+@JvmInline
+value class EmailId(val email: String) {
+    fun isValid() = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$"
+        .toRegex().matches(email)
+}
+
+fun sendEmail(emailId: EmailId): String {
+    return if (emailId.isValid()) {
+        print("Valid")
+        "Sending email to: ${emailId.email}"
+    } else {
+        print("Not Valid")
+        "Invalid email: ${emailId.email}"
+    }
+}
