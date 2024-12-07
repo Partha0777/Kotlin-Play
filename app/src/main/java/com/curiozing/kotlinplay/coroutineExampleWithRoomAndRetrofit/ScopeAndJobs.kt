4c453856469ef0815f815add7635dcc39f4c2f27
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2024-12-07 20:53:28 +0530

    Code cleanup has been done.

--- app/src/main/java/com/curiozing/kotlinplay/coroutineExampleWithRoomAndRetrofit/ScopeAndJobs.kt
+++ app/src/main/java/com/curiozing/kotlinplay/coroutineExampleWithRoomAndRetrofit/ScopeAndJobs.kt
@@ -3,7 +3,6 @@ package com.curiozing.kotlinplay.coroutineExampleWithRoomAndRetrofit
 import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Job
 import kotlinx.coroutines.NonCancellable
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.delay
