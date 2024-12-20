2190c669ceccac0292266e0480ea4fed22e37a9f
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2024-12-20 20:22:03 +0530

    Added lib for list app lifecycles.

--- app/src/main/java/com/curiozing/kotlinplay/appConfig/AppLifecycleListener.kt
+++ app/src/main/java/com/curiozing/kotlinplay/appConfig/AppLifecycleListener.kt
@@ -1,4 +1,16 @@
 package com.curiozing.kotlinplay.appConfig
 
-class AppLifecycleListener {
+import android.util.Log
+import androidx.lifecycle.DefaultLifecycleObserver
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.LifecycleOwner
+
+class AppLifecycleListener : DefaultLifecycleObserver{
+    override fun onStart(owner: LifecycleOwner) {
+        Log.d("AppLifecycle", "App in foreground")
+    }
+
+    override fun onStop(owner: LifecycleOwner) {
+        Log.d("AppLifecycle", "App in background")
+    }
 }
