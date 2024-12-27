3bec4255597ded048f564ba48135fd3579fcfdf0
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2024-12-27 22:27:31 +0530

    Added Generic variance - In type.

--- app/src/main/java/com/curiozing/kotlinplay/generics/InVariance.kt
+++ app/src/main/java/com/curiozing/kotlinplay/generics/InVariance.kt
@@ -1,2 +1,26 @@
 package com.curiozing.kotlinplay.generics
 
+
+class Cage<in T> {
+    fun put(item: T) {
+        println("Putting ${item!!::class.simpleName} in the cage")
+    }
+}
+
+fun putDogInCage(cage: Cage<Dog>) {
+    cage.put(Dog()) // Only allows adding dogs
+}
+
+fun main() {
+    val animalCage: Cage<Animal> = Cage<Animal>()
+    putDogInCage(animalCage)
+}
+
+open class Car()
+class Honda : Car()
+class BMW : Car()
+
+class ShowRoom<out T>(val car:T){
+    fun getCar() = car
+}
+
