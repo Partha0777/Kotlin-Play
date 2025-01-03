632c793897fbcd85a0462e4d27a60fdbc3a16cd1
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2025-01-03 22:39:32 +0530

    Added Example for nested class.

--- app/src/main/java/com/curiozing/kotlinplay/classTypes/NestedClass.kt
+++ app/src/main/java/com/curiozing/kotlinplay/classTypes/NestedClass.kt
@@ -1,17 +1,19 @@
 package com.curiozing.kotlinplay.classTypes
 
-class OuterClass{
+class OuterClass {
     private var name: String = "Ashu"
-    class NestedClass{
+
+    class NestedClass {
         var description: String = "code inside nested class"
         private var id: Int = 101
-        fun foo(){
+        fun foo() {
             //  print("name is ${name}") // cannot access the outer class member
             println("Id is ${id}")
         }
     }
 }
-fun main(args: Array<String>){
+
+fun main(args: Array<String>) {
 // nested class must be initialize
     println(OuterClass.NestedClass().description) // accessing property
     var obj = OuterClass.NestedClass() // object creation
