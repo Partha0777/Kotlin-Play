f5d603099441168762b28dc8d06ba38e45ffa292
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2025-01-03 22:41:43 +0530

    Added Example for inner class.

--- app/src/main/java/com/curiozing/kotlinplay/classTypes/InnerClass.kt
+++ app/src/main/java/com/curiozing/kotlinplay/classTypes/InnerClass.kt
@@ -1,17 +1,19 @@
 package com.curiozing.kotlinplay.classTypes
 
-class OuterClassIn{
-    private  var name: String = "Ashu"
-    inner class  InnerClass{
+class OuterClassIn {
+    private var name: String = "Ashu"
+
+    inner class InnerClass {
         var description: String = "code inside inner class"
         private var id: Int = 101
-        fun foo(){
+        fun foo() {
             println("name is ${name}") // access the outer class member even private
             println("Id is ${id}")
         }
     }
 }
-fun main(args: Array<String>){
+
+fun main(args: Array<String>) {
     println(OuterClassIn().InnerClass().description) // accessing property
     var obj = OuterClassIn().InnerClass() // object creation
     obj.foo() // access member function
