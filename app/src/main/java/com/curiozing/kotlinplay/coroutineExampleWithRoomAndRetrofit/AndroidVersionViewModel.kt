17c484e48e2ac9db0ae93309c3a06064dfe98a57
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2025-03-02 11:42:18 +0530

    Removed unwanted imports.

--- app/src/main/java/com/curiozing/kotlinplay/coroutineExampleWithRoomAndRetrofit/AndroidVersionViewModel.kt
+++ app/src/main/java/com/curiozing/kotlinplay/coroutineExampleWithRoomAndRetrofit/AndroidVersionViewModel.kt
@@ -8,11 +8,8 @@ import androidx.lifecycle.viewModelScope
 import com.curiozing.kotlinplay.MyApplication
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.async
-import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.map
