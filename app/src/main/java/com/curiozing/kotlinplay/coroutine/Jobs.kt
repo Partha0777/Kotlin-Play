67e1e3689de777e7fd311470666215861919ada6
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2025-04-26 13:09:44 +0530

    Added coroutine job example.

---
+++ app/src/main/java/com/curiozing/kotlinplay/coroutine/Jobs.kt
@@ -0,0 +1,26 @@
+package com.curiozing.kotlinplay.coroutine
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.joinAll
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+fun main(): Unit = runBlocking {
+    val parent = CoroutineScope(Dispatchers.Default).launch {
+        val job1 = launch {
+            delay(1000)
+            println("Task 1 done")
+        }
+        val job2 = launch {
+            delay(500)
+            println("Task 2 done")
+        }
+
+        joinAll(job1,job2)
+        println("Parent job done after all child jobs")
+    }
+
+    parent.join()
+}
