6ba1ffd73c8b0190c8941007cf6a0155d5f99f13
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2025-06-06 21:51:08 +0530

    Added flatMapMerge example.

--- app/src/main/java/com/curiozing/kotlinplay/kotlinFlow/intermediateOperator/FlowFlatMapMerge.kt
+++ app/src/main/java/com/curiozing/kotlinplay/kotlinFlow/intermediateOperator/FlowFlatMapMerge.kt
@@ -1,11 +1,13 @@
 package com.curiozing.kotlinplay.kotlinFlow.intermediateOperator
 
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.flatMapMerge
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.runBlocking
 
+@OptIn(ExperimentalCoroutinesApi::class)
 fun main():Unit = runBlocking {
     flowOf(1,2).flatMapMerge {value ->
         flow {
