9d6f737f108918d2a0cffd587fa2f3613b9adf1b
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2025-06-06 21:58:25 +0530

    Added flatMapLatest example.

---
+++ app/src/main/java/com/curiozing/kotlinplay/kotlinFlow/intermediateOperator/FlowFlatMapLatest.kt
@@ -0,0 +1,29 @@
+package com.curiozing.kotlinplay.kotlinFlow.intermediateOperator
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.runBlocking
+
+fun main():Unit = runBlocking {
+
+    val userInput = flow {
+        emit("A")
+        delay(100) // User types quickly
+        emit("AB")
+        delay(100)
+        emit("ABC")
+    }
+
+   userInput
+        .flatMapLatest {query ->
+        flow {
+            delay(300) // Simulated API call
+            emit("Results for: $query")
+        }
+    }.collect{
+        println("$it")
+    }
+}
