a05b0b947e458f42b1d59f6ae2aa15fb76de5a51
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2025-06-07 13:59:12 +0530

    Added flatMapLatest example.

--- app/src/main/java/com/curiozing/kotlinplay/kotlinFlow/intermediateOperator/FlowTransform.kt
+++ app/src/main/java/com/curiozing/kotlinplay/kotlinFlow/intermediateOperator/FlowTransform.kt
@@ -6,16 +6,14 @@ import kotlinx.coroutines.flow.transform
 
 suspend fun main() {
 
-    flowOf(1, 2, 3, 4, 5, 6, 7, 8)
-        .transform {
+    flowOf(1, 2, 3, 4, 5, 6, 7, 8).transform {
             if (it % 2 == 0) {
                 emit(it)
             }
             if (it == 3) {
                 emit(it)
             }
-        }
-        .collect {
+        }.collect {
             println("Data $it")
         }
 }
