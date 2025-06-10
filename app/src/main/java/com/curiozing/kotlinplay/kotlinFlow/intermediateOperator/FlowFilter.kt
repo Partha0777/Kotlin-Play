4eaef0218b9f13ef3f38b8113e227cf0fe14a3e9
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2025-06-10 21:53:53 +0530

    Added flatMapLatest example.

--- app/src/main/java/com/curiozing/kotlinplay/kotlinFlow/intermediateOperator/FlowFilter.kt
+++ app/src/main/java/com/curiozing/kotlinplay/kotlinFlow/intermediateOperator/FlowFilter.kt
@@ -1,6 +1,5 @@
 package com.curiozing.kotlinplay.kotlinFlow.intermediateOperator
 
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.flowOf
 
