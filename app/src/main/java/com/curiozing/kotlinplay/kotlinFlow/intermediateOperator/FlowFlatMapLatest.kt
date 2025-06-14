97294072eb56ac0dac93a1aa7dc86125cd3a85e5
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2025-06-14 11:19:22 +0530

    Added example for flow flatmap

--- app/src/main/java/com/curiozing/kotlinplay/kotlinFlow/intermediateOperator/FlowFlatMapLatest.kt
+++ app/src/main/java/com/curiozing/kotlinplay/kotlinFlow/intermediateOperator/FlowFlatMapLatest.kt
@@ -1,10 +1,12 @@
 package com.curiozing.kotlinplay.kotlinFlow.intermediateOperator
 
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.runBlocking
 
+@OptIn(ExperimentalCoroutinesApi::class)
 fun main():Unit = runBlocking {
 
     val input = flow{
