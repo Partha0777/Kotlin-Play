023802186f806798692910d7e4244489ee4df3be
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2025-06-15 22:15:38 +0530

    Added example for flow flatmap

--- app/src/main/java/com/curiozing/kotlinplay/kotlinFlow/intermediateOperator/FlowFlatMapConcat.kt
+++ app/src/main/java/com/curiozing/kotlinplay/kotlinFlow/intermediateOperator/FlowFlatMapConcat.kt
@@ -8,6 +8,7 @@ import kotlinx.coroutines.runBlocking
 
 @OptIn(ExperimentalCoroutinesApi::class)
 suspend fun main(): Unit = runBlocking {
+
     flowOf(1, 2).flatMapConcat { value ->
         flow {
             emit("$value A")
