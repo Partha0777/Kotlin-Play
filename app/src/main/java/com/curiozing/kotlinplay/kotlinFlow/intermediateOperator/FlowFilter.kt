5972c577e6b89a2f7cc2c11eab77b95ba6c6c0d0
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2025-06-17 21:54:27 +0530

    Added example for flow flatmap

--- app/src/main/java/com/curiozing/kotlinplay/kotlinFlow/intermediateOperator/FlowFilter.kt
+++ app/src/main/java/com/curiozing/kotlinplay/kotlinFlow/intermediateOperator/FlowFilter.kt
@@ -4,6 +4,7 @@ import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.flowOf
 
 suspend fun main() {
+
     flowOf(1, 2, 3, 4, 5, 6, 7)
         .filter {
             it % 2 == 0
@@ -11,4 +12,5 @@ suspend fun main() {
         .collect {
             println("Data $it")
         }
+
 }
