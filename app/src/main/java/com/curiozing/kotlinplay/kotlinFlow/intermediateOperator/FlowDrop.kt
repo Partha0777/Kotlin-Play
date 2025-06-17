04d584cdfff6343974bb963bed7d4ee1be3aa7a6
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2025-06-17 21:54:58 +0530

    Added example for flow flatmap

--- app/src/main/java/com/curiozing/kotlinplay/kotlinFlow/intermediateOperator/FlowDrop.kt
+++ app/src/main/java/com/curiozing/kotlinplay/kotlinFlow/intermediateOperator/FlowDrop.kt
@@ -2,7 +2,6 @@ package com.curiozing.kotlinplay.kotlinFlow.intermediateOperator
 
 import kotlinx.coroutines.flow.drop
 import kotlinx.coroutines.flow.flowOf
-import kotlinx.coroutines.flow.take
 
 suspend fun main() {
     flowOf(1,2,3,4,5)
