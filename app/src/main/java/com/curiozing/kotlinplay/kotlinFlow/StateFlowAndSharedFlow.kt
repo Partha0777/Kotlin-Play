145960d1209751254f4206f914531ed6aee7825f
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2025-07-01 20:51:54 +0530

    Added example for flow flatmap

--- app/src/main/java/com/curiozing/kotlinplay/kotlinFlow/StateFlowAndSharedFlow.kt
+++ app/src/main/java/com/curiozing/kotlinplay/kotlinFlow/StateFlowAndSharedFlow.kt
@@ -1,7 +1,18 @@
 package com.curiozing.kotlinplay.kotlinFlow
 
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 
-fun main(): Unit = runBlocking {
+fun main() {
+    val randomList = mutableListOf(2, 59, 9, 6, 8)
+    val tempList = mutableListOf<Int>()
 
+    while (randomList.isNotEmpty()) {
+        val max = randomList.min()
+        tempList.add(max)
+        randomList.remove(max)
+    }
+
+    println("Max -> $tempList")
 }
