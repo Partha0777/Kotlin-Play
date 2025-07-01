8b0ef5265535bab4bb01d43bfb44113d6463027e
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2025-07-01 20:52:01 +0530

    Added example for flow flatmap

--- app/src/main/java/com/curiozing/kotlinplay/kotlinFlow/StateFlowAndSharedFlow.kt
+++ app/src/main/java/com/curiozing/kotlinplay/kotlinFlow/StateFlowAndSharedFlow.kt
@@ -1,8 +1,5 @@
 package com.curiozing.kotlinplay.kotlinFlow
 
-import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.sync.Mutex
-import kotlinx.coroutines.sync.withLock
 
 fun main() {
     val randomList = mutableListOf(2, 59, 9, 6, 8)
