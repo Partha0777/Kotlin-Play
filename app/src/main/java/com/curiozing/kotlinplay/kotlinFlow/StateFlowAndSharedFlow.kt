5b5b42b9293d3a1642a2ef2030671f72504605d8
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2025-07-01 21:15:20 +0530

    Added example for flow flatmap

--- app/src/main/java/com/curiozing/kotlinplay/kotlinFlow/StateFlowAndSharedFlow.kt
+++ app/src/main/java/com/curiozing/kotlinplay/kotlinFlow/StateFlowAndSharedFlow.kt
@@ -1,18 +1,17 @@
 package com.curiozing.kotlinplay.kotlinFlow
 
 
-
 fun main() {
 
-    val list = listOf(19,8,2,45,7,3)
+    val list = listOf(19, 8, 2, 45, 7, 3)
     val temp = mutableListOf<Int>()
 
     list.forEach {
         var i = 0
-        while (temp.size > i && it > temp[i]){
+        while (temp.size > i && it > temp[i]) {
             i++
         }
-        temp.add(i,it)
+        temp.add(i, it)
     }
 
     print(temp)
