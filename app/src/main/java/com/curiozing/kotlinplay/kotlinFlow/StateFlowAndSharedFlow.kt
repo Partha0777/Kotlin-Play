6c4adbf29d7bd8af2aab8a559f0f2638921ed145
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2025-07-02 22:44:25 +0530

    Added example for flow flatmap

--- app/src/main/java/com/curiozing/kotlinplay/kotlinFlow/StateFlowAndSharedFlow.kt
+++ app/src/main/java/com/curiozing/kotlinplay/kotlinFlow/StateFlowAndSharedFlow.kt
@@ -3,16 +3,21 @@ package com.curiozing.kotlinplay.kotlinFlow
 
 fun main() {
 
+
+    print(heightNumber(0))
+}
+
+fun heightNumber(pos:Int):Int{
     val list = mutableListOf(19, 8, 2, 45, 7, 3)
     val temp = mutableListOf<Int>()
 
     list.forEach {
         var i = 0
-        while (temp.size > i && it > temp[i]) {
+        while (temp.size > i && it < temp[i]) {
             i++
         }
         temp.add(i, it)
     }
 
-    print(temp)
+    return temp[pos]
 }
