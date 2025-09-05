2b9dc3aea99ca883825d4a8234fcb604087222a5
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2025-09-05 20:17:46 +0530

    Added open keyword example.

---
+++ app/src/main/java/com/curiozing/jvmUtils/JvmUtils.java
@@ -0,0 +1,13 @@
+package com.curiozing.jvmUtils;
+
+import static com.curiozing.jvmUtils.KotlinExampleKt.testMethod;
+
+public class JvmUtils {
+
+    void callFunction(){
+
+        Student student = new Student("Kan", 29);
+        String dataExample = TestKotlin.getDataExample();
+        testMethod();
+    }
+}
---
+++ app/src/main/java/com/curiozing/jvmUtils/kotlinExample.kt
@@ -0,0 +1,19 @@
+package com.curiozing.jvmUtils
+
+
+class TestKotlin(){
+
+    companion object{
+
+        @JvmStatic
+        val dataExample = "Test";
+    }
+
+}
+
+@JvmOverloads
+fun testMethod(data: String = "Example data"){
+
+}
+
+data class Student(@JvmField var name: String, var age:Int)
