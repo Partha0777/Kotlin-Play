c10736ce86fedbeef119f184e91dfde4e779b451
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2025-09-07 21:50:15 +0530

    Added JVM Interop example

--- app/src/main/java/com/curiozing/jvmUtils/JvmUtils.java
+++ app/src/main/java/com/curiozing/jvmUtils/JvmUtils.java
@@ -3,7 +3,6 @@ package com.curiozing.jvmUtils;
 import static com.curiozing.jvmUtils.KotlinExampleKt.testMethod;
 
 public class JvmUtils {
-
     void callFunction() {
 
         Student student = new Student("Kan", 29);
