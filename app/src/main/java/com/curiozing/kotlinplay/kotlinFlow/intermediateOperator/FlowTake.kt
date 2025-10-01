ea4994f9c235dc7e53ced388b9ba22d0ea78bdaa
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2025-10-01 22:30:24 +0530

    Added JVM Interop example

--- app/src/main/java/com/curiozing/kotlinplay/kotlinFlow/intermediateOperator/FlowTake.kt
+++ app/src/main/java/com/curiozing/kotlinplay/kotlinFlow/intermediateOperator/FlowTake.kt
@@ -4,9 +4,11 @@ import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.take
 
 suspend fun main() {
+
     flowOf(1, 2, 3, 4, 5)
         .take(3)
         .collect {
             println("Take $it")
         }
+
 }
